96ee47569638661fb3e2a44b80b093a0a926eb7b
David Reaver <johndreaver@gmail.com>
2016-08-17 18:40:43 -0700

    Added a projection store

--- src/EventSourcing/Store.hs
+++ src/EventSourcing/Store.hs
@@ -1,7 +1,9 @@
 module EventSourcing.Store
   ( EventStore (..)
+  , ProjectionStore (..)
   ) where
 
+import EventSourcing.Projection
 import EventSourcing.UUID
 
 -- Is the UUID part really necessary? Should that be handled in a StoredEvent
@@ -21,3 +23,14 @@ class (Monad m) => EventStore store m event | store -> event where
 
 -- newtype SequenceNumber = SequenceNumber { unSequenceNumber :: Int }
 --   deriving (Show, Ord, Eq)
+
+
+class (Projection proj, Monad m) => ProjectionStore store m proj | store -> proj where
+  getProjection :: store -> UUID -> m proj
+  applyEvents :: store -> UUID -> [Event proj] -> m ()
+
+-- data StoredProjection proj
+--   = StoredProjection
+--   { storedProjectionProjection :: proj
+--   , storedProjectionSequenceNumber :: SequenceNumber
+--   } deriving (Show)
