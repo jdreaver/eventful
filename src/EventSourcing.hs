4ad7e2a5e73967b2fb05c20e20588bbac421b53a
David Reaver <johndreaver@gmail.com>
2016-08-22 10:44:16 -0700

    Added a ProjectionMap newtype

--- src/EventSourcing.hs
+++ src/EventSourcing.hs
@@ -5,5 +5,6 @@ module EventSourcing
 import EventSourcing.Aggregate as X
 import EventSourcing.EventBus as X
 import EventSourcing.Projection as X
+import EventSourcing.ProjectionMap as X
 import EventSourcing.Store as X
 import EventSourcing.UUID as X
---
+++ src/EventSourcing/ProjectionMap.hs
@@ -0,0 +1,32 @@
+module EventSourcing.ProjectionMap
+  ( ProjectionMap (..)
+  , projectionMap
+  , applyProjectionMap
+  , lookupProjectionMap
+  ) where
+
+import Data.Map.Strict(Map)
+import qualified Data.Map.Strict as Map
+import Data.Maybe (fromMaybe)
+
+import EventSourcing.Projection
+import EventSourcing.UUID
+
+-- | Holds multiple 'Projection's in memory. Useful for in-memory event stores.
+newtype ProjectionMap proj
+  = ProjectionMap { unProjectionMap :: Map UUID proj }
+  deriving (Show)
+
+projectionMap :: ProjectionMap a
+projectionMap = ProjectionMap Map.empty
+
+applyProjectionMap :: (Projection a) => UUID -> Event a -> ProjectionMap a -> ProjectionMap a
+applyProjectionMap uuid event (ProjectionMap map') =
+  ProjectionMap (Map.insert uuid newAP map')
+  where
+    newAP = case Map.lookup uuid map' of
+      Nothing -> seed
+      Just ap -> apply ap event
+
+lookupProjectionMap :: (Projection proj) => UUID -> ProjectionMap proj -> proj
+lookupProjectionMap uuid (ProjectionMap map') = fromMaybe seed (Map.lookup uuid map')
