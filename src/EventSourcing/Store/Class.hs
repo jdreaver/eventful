55df89aa13eff425a4d84146b5a9b2965eef366e
David Reaver <johndreaver@gmail.com>
2016-08-24 08:48:50 -0700

    Begin splitting up event store classes

--- src/EventSourcing/Store/Class.hs
+++ src/EventSourcing/Store/Class.hs
@@ -1,5 +1,8 @@
 module EventSourcing.Store.Class
-  ( EventStore (..)
+  ( RawEventStore (..)
+  , SequencedEventStore (..)
+  , TypedEventStore (..)
+  , AggregateId (..)
   , Serializable (..)
   , StoredEvent (..)
   , EventVersion (..)
@@ -32,27 +35,37 @@ instance (FromJSON a, ToJSON a) => Serializable a ByteString where
   serialize = encode
   deserialize = decode
 
--- | An event store is anything that stores serialized events in some order
+-- | An raw event store is anything that stores serialized events in some order
 -- based on UUID. This class knows nothing about Projections or how the events
 -- are used, it just marshals them around.
-class (Monad m) => EventStore store m event | store -> event where
+class (Monad m) => RawEventStore m store where
   getUuids :: store -> m [UUID]
   getEvents :: store -> UUID -> m [StoredEvent event]
+  storeEvents :: store -> UUID -> [event] -> m [StoredEvent event]
+
+-- | An event store with some global ordering of events. This is very useful to
+-- help synchronize and/or replay read models without concurrency concerns.
+class (Monad m) => SequencedEventStore store m where
   getAllEvents :: store -> SequenceNumber -> m [StoredEvent event]
 
   -- Some implementations might have a more efficient way to do this.
   getAllEventsPipe :: store -> SequenceNumber -> m (Producer (StoredEvent event) m ())
   getAllEventsPipe store = fmap (mapM_ yield) . getAllEvents store
 
-  -- This can be made way more efficient if the implementation supports
-  -- snapshots.
-  -- getLatestProjection :: store -> UUID -> m proj
-  -- getLatestProjection store uuid = do
-  --   events <- getEvents store uuid
-  --   return $ latestProjection (storedEventEvent <$> events)
+class (Monad m, RawEventStore m store) => TypedEventStore m store where
+  getAggregateEvents :: store -> AggregateId proj -> m [StoredEvent (Event proj)]
+  storeAggregateEvents :: store -> AggregateId proj -> [Event proj] -> m [StoredEvent (Event proj)]
 
-  storeEvents :: store -> UUID -> [event] -> m [StoredEvent event]
-  latestEventVersion :: store -> UUID -> m EventVersion
+  getAggregate :: (Projection proj) => store -> AggregateId proj -> m proj
+  getAggregate store (AggregateId uuid) = do
+    events <- getEvents store uuid
+    return $ latestProjection (storedEventEvent <$> events)
+
+-- | This type ensures our stored events have the correct type, but it also
+-- allows us to avoid type ambiguity errors in event stores by providing the
+-- phantom projection type.
+newtype AggregateId proj = AggregateId { unAggregateId :: UUID }
+  deriving (Show, Eq, ToJSON, FromJSON, PersistField, PersistFieldSql, FromHttpApiData)
 
 data StoredEvent event
   = StoredEvent
@@ -69,7 +82,7 @@ newtype SequenceNumber = SequenceNumber { unSequenceNumber :: Int }
   deriving (Show, Read, Ord, Eq, Enum, Num, FromJSON, ToJSON, PersistField, PersistFieldSql,
             PathPiece, ToHttpApiData, FromHttpApiData)
 
-class (Projection proj, Monad m) => ProjectionStore store m proj | store -> proj where
+class (Projection proj, Monad m) => ProjectionStore m store proj | store -> proj where
   latestApplied :: store -> m SequenceNumber
   getProjection :: store -> UUID -> m proj
   applyEvents :: store -> [StoredEvent (Event proj)] -> m ()
