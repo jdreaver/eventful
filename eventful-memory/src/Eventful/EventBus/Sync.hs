e61c69a17d8aa3712ba55f25e2cbd0a07f365254
David Reaver <johndreaver@gmail.com>
2016-09-15 13:53:50 -0700

    Add a synchronous event bus

---
+++ eventful-memory/src/Eventful/EventBus/Sync.hs
@@ -0,0 +1,25 @@
+module Eventful.EventBus.Sync
+  ( SyncEventBusIO
+  , syncEventBusIO
+  ) where
+
+import Control.Monad.IO.Class
+import Data.IORef
+
+import Eventful.EventBus.Class
+import Eventful.Store
+
+newtype SyncEventBusIO m serialized = SyncEventBusIO (IORef [EventBusHandler m serialized])
+
+syncEventBusIO :: IO (SyncEventBusIO m serialized)
+syncEventBusIO = SyncEventBusIO <$> newIORef []
+
+instance (MonadIO m) => EventBus m (SyncEventBusIO m serialized) serialized where
+  publishEvent (SyncEventBusIO handlersRef) event = do
+    handlers <- liftIO $ readIORef handlersRef
+    mapM_ ($ event) handlers
+
+  registerStoreHandlerStart (SyncEventBusIO handlersRef) seqNum store handler = do
+    startEvents <- getSequencedEvents store seqNum
+    mapM_ handler startEvents
+    liftIO $ modifyIORef handlersRef (\handlers -> handler : handlers)
