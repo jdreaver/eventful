c7d36be22f71ae10efa90af8135e628143c573f3
David Reaver <johndreaver@gmail.com>
2017-02-05 11:30:52 -0800

    Created SQL table for cafe tabs

---
+++ examples/cafe/src/Cafe/DB.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Cafe.DB
+  ( openTab
+  , getTabUuid
+  , migrateTabEntity
+  ) where
+
+import Control.Monad.IO.Class
+import Database.Persist
+import Database.Persist.Sql
+import Database.Persist.TH
+
+import Eventful
+import Eventful.Store.Sqlite()
+
+share [mkPersist sqlSettings, mkMigrate "migrateTabEntity"] [persistLowerCase|
+TabEntity sql=tabs
+    projectionId UUID
+    deriving Show
+|]
+
+-- | Opens a tab by inserting an entry into the tabs table and returning the
+-- UUID.
+openTab :: (MonadIO m) => SqlPersistT m UUID
+openTab = do
+  uuid <- liftIO uuidNextRandom
+  insert_ (TabEntity uuid)
+  return uuid
+
+-- | Given the tab id, attempts to load the tab and return the UUID.
+getTabUuid :: (MonadIO m) => TabEntityId -> SqlPersistT m (Maybe UUID)
+getTabUuid tabId = fmap tabEntityProjectionId <$> get tabId
