1c3cbd8ae739b86db50c34b79235046c4744064e
David Reaver <johndreaver@gmail.com>
2017-03-18 15:33:20 -0700

    Add Docker container for DynamoDB tests

--- run-tests-docker.sh
+++ run-tests-docker.sh
@@ -2,10 +2,9 @@
 
 set -eux;
 
-# Spins up a docker container to run postgres tests
-
+# Spin up a docker container to run postgres tests
 if [ ! "$(docker ps -q -f name=eventful_test_pg)" ]; then
-  if [ "$(docker ps -aq -f status=exited -f name=eventful_test_pg)" ]; then
+  if [ "$(docker ps -aq -f name=eventful_test_pg)" ]; then
     docker rm eventful_test_pg
   fi
   docker run --name eventful_test_pg -e POSTGRES_DB=eventful_test -p 5432:5432/tcp -d postgres:9.6 postgres
@@ -14,4 +13,17 @@ if [ ! "$(docker ps -q -f name=eventful_test_pg)" ]; then
   sleep 10
 fi
 
-stack test --pedantic
+# Spin up a docker container for dynamodb
+if [ ! "$(docker ps -q -f name=eventful_test_dynamo)" ]; then
+  if [ "$(docker ps -aq -f name=eventful_test_dynamo)" ]; then
+    docker rm eventful_test_dynamo
+  fi
+  docker run -d --name eventful_test_dynamo -p 8000:8000 dwmkerr/dynamodb
+fi
+
+# Set dummy env vars for dynamo
+export AWS_ACCESS_KEY_ID=dummy
+export AWS_SECRET_ACCESS_KEY=dummy
+export AWS_DEFAULT_REGION=us-east-1
+
+stack test --pedantic "$@"
