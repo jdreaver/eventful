ef18a2031ff72796ce209acda944c43e6dc69e2e
David Reaver <johndreaver@gmail.com>
2017-04-03 17:26:28 -0700

    Add function to compose serializers

--- eventful-core/src/Eventful/Serializer.hs
+++ eventful-core/src/Eventful/Serializer.hs
@@ -1,6 +1,7 @@
 module Eventful.Serializer
   ( Serializer (..)
   , simpleSerializer
+  , composeSerializers
   , jsonSerializer
   , jsonTextSerializer
   , dynamicSerializer
@@ -32,6 +33,15 @@ simpleSerializer serialize' deserialize' =
   , deserialize = deserialize'
   , deserializeEither = maybe (Left "Serializable: Failed to deserialize") Right . deserialize'
   }
+
+-- | Apply an intermediate 'Serializer' to a serializer to go from type @a@ to
+-- @c@ with @b@ in the middle.
+composeSerializers :: Serializer a b -> Serializer b c -> Serializer a c
+composeSerializers serializer1 serializer2 = Serializer serialize' deserialize' deserializeEither'
+  where
+    serialize' = serialize serializer2 . serialize serializer1
+    deserialize' x = deserialize serializer2 x >>= deserialize serializer1
+    deserializeEither' x = deserializeEither serializer2 x >>= deserializeEither serializer1
 
 -- | A 'Serializer' for aeson 'Value's
 jsonSerializer :: (ToJSON a, FromJSON a) => Serializer a Value
