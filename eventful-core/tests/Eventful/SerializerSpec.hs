259cbae364f4be4760b33689c5c4c25f0dc4e125
David Reaver <johndreaver@gmail.com>
2017-04-11 16:44:04 -0700

    Add tests for sum type serializer

--- eventful-core/tests/Eventful/SerializerSpec.hs
+++ eventful-core/tests/Eventful/SerializerSpec.hs
@@ -6,20 +6,6 @@ import Test.Hspec
 
 import Eventful.Serializer
 
-spec :: Spec
-spec = do
-  describe "EventSumType" $ do
-    it "can serialize events without the constructor" $ do
-      dynTypeRep (eventToDyn $ MyEventsEventA EventA) `shouldBe` typeOf EventA
-      dynTypeRep (eventToDyn $ MyEventsEventA EventA) `shouldBe` dynTypeRep (eventToDyn $ AllEventsEventA EventA)
-
-    it "can deserialize events with the constructor" $ do
-      eventFromDyn (toDyn EventA) `shouldBe` Just (MyEventsEventA EventA)
-      eventFromDyn (toDyn EventB) `shouldBe` Just (AllEventsEventB EventB)
-
-      eventFromDyn (eventToDyn $ MyEventsEventA EventA) `shouldBe` Just (AllEventsEventA EventA)
-      eventFromDyn (eventToDyn $ AllEventsEventB EventB) `shouldBe` Just (MyEventsEventB EventB)
-
 data EventA = EventA deriving (Show, Eq)
 data EventB = EventB deriving (Show, Eq)
 data EventC = EventC deriving (Show, Eq)
@@ -38,3 +24,29 @@ data MyEvents
   deriving (Show, Eq, Generic)
 
 instance EventSumType MyEvents
+
+mkSumTypeSerializer "myEventsSerializer" ''MyEvents ''AllEvents
+
+spec :: Spec
+spec = do
+  describe "EventSumType" $ do
+    it "can serialize events without the constructor" $ do
+      dynTypeRep (eventToDyn $ MyEventsEventA EventA) `shouldBe` typeOf EventA
+      dynTypeRep (eventToDyn $ MyEventsEventA EventA) `shouldBe` dynTypeRep (eventToDyn $ AllEventsEventA EventA)
+
+    it "can deserialize events with the constructor" $ do
+      eventFromDyn (toDyn EventA) `shouldBe` Just (MyEventsEventA EventA)
+      eventFromDyn (toDyn EventB) `shouldBe` Just (AllEventsEventB EventB)
+
+      eventFromDyn (eventToDyn $ MyEventsEventA EventA) `shouldBe` Just (AllEventsEventA EventA)
+      eventFromDyn (eventToDyn $ AllEventsEventB EventB) `shouldBe` Just (MyEventsEventB EventB)
+
+  describe "mkSumTypeSerializer" $ do
+    it "can serialize events" $ do
+      serialize myEventsSerializer (MyEventsEventA EventA) `shouldBe` AllEventsEventA EventA
+      serialize myEventsSerializer (MyEventsEventB EventB) `shouldBe` AllEventsEventB EventB
+
+    it "can deserialize events" $ do
+      deserialize myEventsSerializer (AllEventsEventA EventA) `shouldBe` Just (MyEventsEventA EventA)
+      deserialize myEventsSerializer (AllEventsEventB EventB) `shouldBe` Just (MyEventsEventB EventB)
+      deserialize myEventsSerializer (AllEventsEventC EventC) `shouldBe` Nothing
